44cd63e849f57fecd71b7807b006da60a9e05d56
Daniel Schaefer <dhs@frame.work>
2024-08-14 16:12:26 +0800

    python: Fix debug_print
    Signed-off-by: Daniel Schaefer <dhs@frame.work>

--- python/qmk_hid/gui.py
+++ python/qmk_hid/gui.py
@@ -112,7 +112,7 @@ RGB_EFFECTS = [
     "SOLID_MULTISPLASH",
 ]
 
-def debug_print(args=""):
+def debug_print(*args):
     if DEBUG_PRINT:
         print(args)
 
@@ -260,7 +260,7 @@ def main():
         # print('Values', values)
 
         for dev in devices:
-            debug_print("Dev {} is {}".format(dev['product_string'], dev.get('disconnected')))
+            debug_print("Dev '{}' disconnected: {}".format(dev['product_string'], 'disconnected' in dev))
             if 'disconnected' in dev:
                 window['-CHECKBOX-{}-'.format(dev['path'])].update(False, disabled=True)
 
@@ -508,6 +508,7 @@ def find_devs(show, verbose):
                 print(f"Serial No:    {sn}")
 
             if verbose:
+                print(f"Path:         {path}")
                 print(f"VID/PID:      {vid:02X}:{pid:02X}")
                 print(f"Interface:    {interface}")
                 # TODO: print Usage Page
@@ -544,7 +545,7 @@ def send_message(dev, message_id, msg, out_len):
         return out_data
     except (IOError, OSError) as ex:
         dev['disconnected'] = True
-        debug_print("Error: ", ex)
+        debug_print("Error ({}): ".format(dev['path']), ex)
         # Doesn't actually exit the process, pysimplegui catches it
         # But it avoids the return value being used
         # TODO: Get rid of this ugly hack and properly make the caller handle the failure
